8dfb6bfc80929a1eb56c5956b71b86fe0cd361bd
Jahid Uddin <saklan420@gmail.com>
2026-08-19 19:02:16 +0600

    Implement basic arithmetic operations with validation

---
+++ if-else3.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/W
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char s, q;
+
+    cin >> a >> s >> b >> q >> c;
+
+    int ans;
+
+    if (s == '+') {
+        ans = a + b;
+    }
+    else if (s == '-') {
+        ans = a - b;
+    }
+    else {
+        ans = a * b;
+    }
+
+    if (ans == c) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << ans << endl;
+    }
+
+    return 0;
+}
