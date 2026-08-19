d2b98f09cc75d628161cbc449c8d1ade87b30861
Jahid Uddin <saklan420@gmail.com>
2026-08-19 21:51:45 +0600

    Add prime number checker and display up to n

---
+++ function5.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+#include <bits/stdc++.h>
+using namespace std;
+
+bool pri(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        if (pri(i))
+            cout << i << " ";
+    }
+
+    return 0;
+}
